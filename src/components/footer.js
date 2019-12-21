ece183c3829288f4247ddd84748518fa25576989
irisjitomo <irisjitomo@eyahoo.com>
2019-12-20 23:05:53 -0500

    made final mobile adjustments

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,18 +13,6 @@ const Footer = () => {
 				<div className="footer-contact-card">
 					<p>© Iris Jitomo 2019. All Rights Reserved.</p>
 				</div>
-				{/* <div className="contact-cards-info">
-					<a href="mailto:irisjitomo@yahoo.com" target="_blank">
-						<img src={email} alt="send email" />
-					</a>
-					<a href="https://github.com/irisjitomo" target="_blank">
-						<img src={gitHub} alt="gitHub" />
-					</a>
-					<a href="https://www.linkedin.com/in/iris-jitomo-ab891b191/" target="_blank">
-						<img src={linkedIn} alt="linkedIn" />
-					</a>
-				</div> */}
-                {/* <br /> */}
                 <div className="miscellaneous">
                 <a target="_blank" href="/icons/set/html-5">Html 5</a>, <a target="_blank" href="/icons/set/css3">CSS3</a> and other icons by <a target="_blank" href="https://icons8.com">Icons8</a>
                 </div>
